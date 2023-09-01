0a6b175ce22fecbd66c5ca22f7369ab3f8b3a3d5
alirezaseif20 <alirezaariye@gmail.com>
2023-09-01 18:14:56 +0430

    feat: add searchBox widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_list/constants/colors.dart';
 import 'package:to_do_list/widgets/app_bar.dart';
+import 'package:to_do_list/widgets/search_box.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: tdBGColor, appBar: buildAppBar());
+    return Scaffold(
+      backgroundColor: tdBGColor,
+      appBar: buildAppBar(),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          children: [
+            searchBox(),
+          ],
+        ),
+      ),
+    );
   }
 }
