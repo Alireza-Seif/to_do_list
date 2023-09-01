f34fa22042ae4d11f35e75b818730ec1228b7647
alirezaseif20 <alirezaariye@gmail.com>
2023-09-01 23:52:38 +0430

    feat: create To Do Item

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:to_do_list/constants/colors.dart';
-import 'package:to_do_list/widgets/app_bar.dart';
-import 'package:to_do_list/widgets/search_box.dart';
+
+import '../constants/colors.dart';
+import '../model/todo.dart';
+import '../widgets/app_bar.dart';
+import '../widgets/search_box.dart';
+import '../widgets/todo_item.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  final todosList = ToDo.todoList();
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +17,30 @@ class HomePage extends StatelessWidget {
       backgroundColor: tdBGColor,
       appBar: buildAppBar(),
       body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 15),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
         child: Column(
           children: [
             searchBox(),
+            Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(top: 50, bottom: 20),
+                    child: const Text(
+                      'Seif List',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  for (ToDo todoo in todosList)
+                    ToDoItem(
+                      todo: todoo,
+                    ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
